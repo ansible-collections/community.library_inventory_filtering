b510c5605c37bd3ece19f77c6688549745228c3c
Felix Fontein <felix@fontein.de>
2025-08-17 19:22:52 +0200

    Avoid deprecated functionality.

--- plugins/plugin_utils/inventory_filter.py
+++ plugins/plugin_utils/inventory_filter.py
@@ -10,7 +10,7 @@ from __future__ import absolute_import, division, print_function
 __metaclass__ = type
 
 from ansible.errors import AnsibleError, AnsibleParserError
-from ansible.module_utils.common._collections_compat import Mapping
+from ansible.module_utils.six.moves.collections_abc import Mapping
 from ansible.module_utils.common.text.converters import to_native
 from ansible.module_utils.parsing.convert_bool import boolean
 from ansible.module_utils.six import string_types
